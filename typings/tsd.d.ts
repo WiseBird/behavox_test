27aaf002e3c491ef60bc8ddc416942bb92182c9e
WiseBird <sokolcjrjk@gmail.com>
2016-05-20 11:39:26 +0300

    Added typings for bower packages

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angularjs/angular-animate.d.ts" />
+/// <reference path="angularjs/angular-mocks.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
